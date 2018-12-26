3d3aa7a37c8c921d0aac67faefad59fee555e909
Fabien JUIF <fabien.juif@gmail.com>
2018-12-26 13:33:30 +0100

    :art: add todo about cardinal product

--- src/commit.rs
+++ src/commit.rs
@@ -18,6 +18,7 @@ impl Commit {
             Some(captures) => {
                 let emoji_code = captures.get(1).unwrap().as_str();
                 let summary = captures.get(2).unwrap().as_str().trim();
+                // TODO: use a HashMap instead of doing this cardinal product
                 let group_code = match groups.iter().find(|group| group.codes.iter().any(|&code| code == emoji_code)) {
                     None => String::from("Others"),
                     Some(group) => group.name.to_string(),
