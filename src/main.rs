bd22fb18e971ad4769f2051e9b234ec8effa7f37
Fabien JUIF <fabien.juif@gmail.com>
2018-12-26 19:23:28 +0100

    write on file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate serde_json;
 #[macro_use]
 extern crate lazy_static;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 use std::env;
 
 use handlebars::Handlebars;
@@ -39,16 +42,11 @@ fn main() {
             .takes_value(true))
         .arg(Arg::with_name("path")
             .help("path to the git repository to parse")
-            .index(1)
+            .value_name("GIT_REPOSITORY_PATH")
             .required(true))
         .get_matches();
 
-    println!("Git repository path: {}", matches.value_of("path").unwrap());
-
-    match matches.value_of("output") {
-        Some(path) => println!("Output on file: {}", path),
-        None => println!("Writing on stdout"),
-    }
+    eprintln!("Git repository path: {}", matches.value_of("path").unwrap());
 
     let repository = env::args().nth(1).unwrap();
 
@@ -56,9 +54,14 @@ fn main() {
     // let changelog = Changelog::from_range(&repository, "06a218d4bba6d3d7bf359bd9eff4013f585fc1fa..HEAD");
     let changelog = Changelog::from_range(&repository, "v1.0.0..HEAD");
 
-    // let mut reg = Handlebars::new();
-    // println!(
-    //     "{}",
-    //     reg.render_template(TEMPLATE, &json!(changelog)).unwrap(),
-    // );
+    let mut reg = Handlebars::new();
+    let result = reg.render_template(TEMPLATE, &json!(changelog)).unwrap();
+
+    match matches.value_of("output") {
+        None => println!("{}", result),
+        Some(path) => {
+            let mut file = File::create(path).unwrap();
+            file.write_all(result.as_bytes()).unwrap();
+        }
+    }
 }
