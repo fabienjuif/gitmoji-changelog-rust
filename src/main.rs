f7daa69bace8b5cf86bfb5216deaff93198fa7e9
Fabien JUIF <fabien.juif@gmail.com>
2018-12-26 19:23:44 +0100

    :sparkles: cli options

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ extern crate serde_json;
 #[macro_use]
 extern crate lazy_static;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 use std::env;
 
 use handlebars::Handlebars;
+use clap::{Arg, App, SubCommand};
 
 mod group;
 mod commit;
@@ -27,6 +31,23 @@ const TEMPLATE: &str = r"
 ";
 
 fn main() {
+    let matches = App::new("gitmoji-changelog")
+        .version("1.0.0")
+        .author("Fabien JUIF <fabien.juif@gmail.com>")
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("FILE")
+            .help("File to update, if not defined write on stdout")
+            .takes_value(true))
+        .arg(Arg::with_name("path")
+            .help("path to the git repository to parse")
+            .value_name("GIT_REPOSITORY_PATH")
+            .required(true))
+        .get_matches();
+
+    eprintln!("Git repository path: {}", matches.value_of("path").unwrap());
+
     let repository = env::args().nth(1).unwrap();
 
     // let changelog = Changelog::from_range(&repository, "06a218d4bba6d3d7bf359bd9eff4013f585fc1fa..44b21e9d4b040ba4f36ce136c82a59659a68701b");
@@ -34,10 +55,13 @@ fn main() {
     let changelog = Changelog::from_range(&repository, "v1.0.0..HEAD");
 
     let mut reg = Handlebars::new();
-    println!(
-        "{}",
-        reg.render_template(TEMPLATE, &json!(changelog)).unwrap(),
-    );
+    let result = reg.render_template(TEMPLATE, &json!(changelog)).unwrap();
 
-    // println!("{:#?}", changelog);
+    match matches.value_of("output") {
+        None => println!("{}", result),
+        Some(path) => {
+            let mut file = File::create(path).unwrap();
+            file.write_all(result.as_bytes()).unwrap();
+        }
+    }
 }
