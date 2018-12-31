b2d1cec2baff207256b37ad7853fb903724fa1ff
Fabien JUIF <fabien.juif@gmail.com>
2018-12-31 17:25:56 +0100

    :sparkles: lib

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
+
+pub mod changelog;
+pub mod commit;
+pub mod group;
+pub mod version;
